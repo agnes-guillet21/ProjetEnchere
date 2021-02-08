dc2e68ace1de4b10aabbf42223b47470b32cae9d
Thomas <Thomas@DESKTOP-PP5NAOS>
2021-02-08 16:49:21 +0100

    Classe categorie et retrait

---
+++ src/ProjetEnchere/bo/Categorie.java
@@ -0,0 +1,39 @@
+package ProjetEnchere.bo;
+
+
+public class Categorie implements Serializable {
+    private int noCategorie; // auto généré  
+    private String libelle;
+
+    
+    public Categorie() {
+    }
+
+    public Categorie(int noCategorie, String libelle) {
+        this.noCategorie = noCategorie;
+        this.libelle = libelle;
+    }
+
+    public int getNoCategorie() {
+        return noCategorie;
+    }
+
+    public void setNoCategorie(int noCategorie) {
+        this.noCategorie = noCategorie;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String toString() {
+        return "Categorie{" +
+                "noCategorie=" + noCategorie +
+                ", libelle='" + libelle + '\'' +
+                '}';
+    }
+}
