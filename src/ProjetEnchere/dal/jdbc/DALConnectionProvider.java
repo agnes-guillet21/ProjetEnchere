2806f8415ccc168676f0faeb399b91b8e76efe3e
Thomas <Thomas@DESKTOP-PP5NAOS>
2021-02-09 11:46:39 +0100

    TL ConnectionProvider

---
+++ src/ProjetEnchere/dal/jdbc/DALConnectionProvider.java
@@ -0,0 +1,31 @@
+package ProjetEnchere.dal.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public abstract class DALConnectionProvider {
+	
+	private static DataSource dataSource;
+	
+	static
+	{
+		Context context;
+		try {
+			context = new InitialContext();
+			DALConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx");
+		} catch (NamingException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Impossible d'accéder à la base de données");
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException
+	{
+		return DALConnectionProvider.dataSource.getConnection();
+	}
+}
