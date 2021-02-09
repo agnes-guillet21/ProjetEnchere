299b14f7cb2936fb9e58d6015d138aea69d7f0f9
amaur <amaur@DESKTOP-TCEDOAQ>
2021-02-09 12:14:41 +0100

    AP creation context.xml

--- src/ProjetEnchere/dal/jdbc/DALConnectionProvider.java
+++ src/ProjetEnchere/dal/jdbc/DALConnectionProvider.java
@@ -20,7 +20,7 @@ public abstract class DALConnectionProvider {
 			DALConnectionProvider.dataSource = (DataSource)context.lookup("java:comp/env/jdbc/pool_cnx");
 		} catch (NamingException e) {
 			e.printStackTrace();
-			throw new RuntimeException("Impossible d'accÃ©der Ã  la base de donnÃ©es");
+			throw new RuntimeException("Impossible d'accéder à  la base de données");
 		}
 	}
 	
--- src/ProjetEnchere/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/ProjetEnchere/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -1,7 +1,22 @@
 package ProjetEnchere.dal.jdbc;
 
+import ProjetEnchere.bo.Utilisateur;
 import ProjetEnchere.dal.UtilisateurDAO;
 
 public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
+
+	@Override
+	public void insert(UtilisateurDAO u1) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UtilisateurDAO selectByPseudo(String pseudo) throws DALException {
+		UtilisateurDAO u1 = ;
+		
+		
+		return u1;
+	}
 
 }
