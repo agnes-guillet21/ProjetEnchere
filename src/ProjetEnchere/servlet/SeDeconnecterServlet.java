1901bfbde048e73b3de779ec5f586c14f7e77cae
Thomas <Thomas@DESKTOP-PP5NAOS>
2021-02-09 21:43:40 +0100

    TL SeDeconnecterServlet

---
+++ src/ProjetEnchere/servlet/SeDeconnecterServlet.java
@@ -0,0 +1,40 @@
+package ProjetEnchere.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/SeDeconnecterServlet")
+public class SeDeconnecterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	 public SeDeconnecterServlet() {
+	        super();
+	        // TODO Auto-generated constructor stub
+}
+	 
+	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+		 
+	 // suppression session 
+	 HttpSession session = request.getSession();
+		session.removeAttribute("Utilisateur");
+		session.invalidate();
+		
+		// retour acceuil
+		this.getServletContext().getRequestDispatcher("/ProjetEnchere").forward(request, response); 
+}
+
+
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	doGet(request, response);
+	
+	 }
+
+}
