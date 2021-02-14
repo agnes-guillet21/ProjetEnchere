746e7f8f04798642191743ef2d8ed4bcf227e667
aguillet2020 <aguillet2020@22SE44-63MRZS2.ad.campus-eni.fr>
2021-02-14 16:54:02 +0100

    ag

---
+++ src/ProjetEnchere/bll/RetraitManager.java
@@ -0,0 +1,21 @@
+package ProjetEnchere.bll;
+
+import ProjetEnchere.bo.Retrait;
+
+public class RetraitManager {
+
+	public RetraitManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Retrait selectByCriteres(Retrait r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void insert(Retrait r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
