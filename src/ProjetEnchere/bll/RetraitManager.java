866ab7e8e89ff6a4ecdec7e65e93a18b1eb2d94b
amaur <amaur@DESKTOP-TCEDOAQ>
2021-02-14 16:03:14 +0100

    AP EtatVente RetraitManager

--- src/ProjetEnchere/bll/RetraitManager.java
+++ src/ProjetEnchere/bll/RetraitManager.java
@@ -1,5 +1,17 @@
 package ProjetEnchere.bll;
 
+import ProjetEnchere.bo.Retrait;
+
 public class RetraitManager {
+
+	/**
+	 * Méthode permettant de sélectionner et récupérer un lieu de Retrait à partir de la rue, du code postal et de la ville dans la base de données
+	 * @param Retrait r
+	 * @return Retrait r ou null si le lieu n'existe pas en base de données
+	 */
+	public Retrait selectByCriteres(Retrait r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/ProjetEnchere/bo/EtatVente.java
+++ src/ProjetEnchere/bo/EtatVente.java
@@ -3,5 +3,7 @@ package ProjetEnchere.bo;
 public class EtatVente {
 
 	public static final String EN_COURS = "En cours";
+	public static final String TERMINEES = "Terminée";
+	public static final String ANNULEES = "Annulée";
 
 }
