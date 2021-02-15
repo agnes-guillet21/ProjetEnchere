16c852508c6a14d34bc36329942f6b65ae3f7ac4
amaur <amaur@DESKTOP-TCEDOAQ>
2021-02-15 10:08:16 +0100

    AP ArticleVenduDAO

--- src/ProjetEnchere/dal/ArticleVenduDAO.java
+++ src/ProjetEnchere/dal/ArticleVenduDAO.java
@@ -9,40 +9,31 @@ import ProjetEnchere.dal.jdbc.DALException;
 public interface ArticleVenduDAO {
 	
 	/**
-<<<<<<< HEAD
 	 * Méthode permettant de lister toutes les ventes de l'application
 	 * @return List<ArticleVendu> Une liste d'objets de type ArticleVendu
 	 * @throws DALException
-=======
-	 * Méthode permettant d'afficher la liste de toutes les ventes
-	 * @return une liste d'objet de types ArticleVendu
-	 * @throws BLLException
->>>>>>> branch 'main' of https://github.com/agnes-guillet21/ProjetEnchere
 	 */
 	List<ArticleVendu> listerToutesLesVentes()throws DALException;
 	
-<<<<<<< HEAD
+
 	/**
 	 * Méthode permettant de lister les ventes en fonctions de critères donnés par l'utilisateur
 	 * @return List<ArticleVendu> Une liste d'objets de type ArticleVendu
 	 * @throws DALException
 	 */
-=======
-	
->>>>>>> branch 'main' of https://github.com/agnes-guillet21/ProjetEnchere
 	List<ArticleVendu> listerVentesParCriteres()throws DALException;
 	
 	/**
-<<<<<<< HEAD
 	 * Méthode permettant d'ajouter une vente (un ArticleVendu) dans la base de donnée
 	 * @param Une instance d'ArticleVendu aV
 	 * @throws DALException
 	 */
 	void ajouterVente(ArticleVendu aV) throws DALException;
-=======
+
+	/**
 	 * Méthode permettant d'insérer une nouvelle vente (objet de type ArticleVendu) dans la base de donnée
 	 * @param ArticleVendu a
 	 */
 	void insert(ArticleVendu a);
->>>>>>> branch 'main' of https://github.com/agnes-guillet21/ProjetEnchere
+
 }
