4e44ffa5615a0dfa617a2dbb5ea9279d2e7fe1ac
aguillet2020 <aguillet2020@22SE44-63MRZS2.ad.campus-eni.fr>
2021-02-15 10:09:48 +0100

    tiens pr toi mamou

--- src/ProjetEnchere/servlet/CreerVenteServlet.java
+++ src/ProjetEnchere/servlet/CreerVenteServlet.java
@@ -1,40 +1,15 @@
 package ProjetEnchere.servlet;
 
 import java.io.IOException;
-<<<<<<< HEAD
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CreerVenteServlet
- */
-@WebServlet("/vendre.html")
-public class CreerVenteServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreerVenteServlet() {
-        super();
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/vendrearticle.jsp").forward(request, response);
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-=======
 import java.time.LocalDate;
 
 import javax.servlet.RequestDispatcher;
@@ -112,7 +87,7 @@ public class CreerVenteServlet extends HttpServlet {
 		
 		rd = request.getRequestDispatcher("/ProjetEnchere");
 		rd.forward(request, response);
->>>>>>> branch 'main' of https://github.com/agnes-guillet21/ProjetEnchere
+
 	}
 
 }
